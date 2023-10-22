ffb4cf6cee64d4180086a48d789ca42763af300d
Michalina Hytrek <michalinahytrek@MacBook-Pro-Michalina.local>
2023-10-22 16:11:56 +0200

    dodanie Vector2d.java

---
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -0,0 +1,93 @@
+package agh.ics.oop.model;
+
+import java.util.Vector;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return "(" + this.x + "," + this.y +")";
+    }
+
+    boolean precedes(Vector2d other){
+        if(other.x >= this.x && other.y >= this.y){
+            return true;
+        } else return false;
+    }
+
+    boolean follows(Vector2d other){
+        if(other.x <= this.x && other.y <= this.y){
+            return true;
+        } else return false;
+    }
+
+    Vector2d add(Vector2d other){
+        int x3 = this.x + other.x;
+        int y3 = this.y + this.x;
+        return new Vector2d(x3, y3);
+    }
+
+    Vector2d subtract(Vector2d other){
+        int x3 = this.x - other.x;
+        int y3 = this.y - this.x;
+        return new Vector2d(x3, y3);
+    }
+
+    Vector2d upperRight(Vector2d other){
+        int x4;
+        int y4;
+        if(this.x >= other.x){
+            x4 = this.x;
+        }
+        else {
+            x4 = other.x;
+        }
+        if(this.y >= other.y){
+            y4 = this.y;
+        }
+        else {
+            y4 = other.y;
+        }
+        return new Vector2d( x4, y4);
+    }
+
+    Vector2d lowerLeft(Vector2d other){
+        int x4;
+        int y4;
+        if(this.x <= other.x){
+            x4 = this.x;
+        }
+        else {
+            x4 = other.x;
+        }
+        if(this.y <= other.y){
+            y4 = this.y;
+        }
+        else {
+            y4 = other.y;
+        }
+        return new Vector2d( x4, y4);
+    }
+
+    Vector2d opposite(){
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Vector2d)) {
+            return false;
+        }
+        Vector2d that = (Vector2d) other;
+        if (that.x == this.x && that.y == this.y) {
+            return true;
+        } else return false;
+    }
+}
