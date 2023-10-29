af1d19a5630be58f9a005a2bd11c2c08e036a9c9
Michalina Hytrek <michalinahytrek@MacBook-Pro-Michalina.local>
2023-10-29 17:41:38 +0100

    Zmiany w OptionParserTest zmiana na LinkList

--- oolab/src/test/java/agh/ics/oop/model/OptionParserTest.java
+++ oolab/src/test/java/agh/ics/oop/model/OptionParserTest.java
@@ -3,6 +3,9 @@ package agh.ics.oop.model;
 import agh.ics.oop.OptionsParser;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class OptionParserTest {
@@ -11,27 +14,26 @@ public class OptionParserTest {
     public void testOptionParser(){
         //given
         String[] list_1 = {"b", "f", "r", "l"};
-        MoveDirection[] expected_1 = {MoveDirection.BACKWARD, MoveDirection.FORWARD, MoveDirection.RIGHT, MoveDirection.LEFT};
+        LinkedList<MoveDirection> expected_1 = new LinkedList<>(List.of(MoveDirection.BACKWARD, MoveDirection.FORWARD, MoveDirection.RIGHT, MoveDirection.LEFT));
         String[] list_2 = {"f", "f", "b"};
-        MoveDirection[] expected_2 = {MoveDirection.FORWARD, MoveDirection.FORWARD, MoveDirection.BACKWARD};
+        LinkedList<MoveDirection> expected_2 = new LinkedList<>(List.of(MoveDirection.FORWARD, MoveDirection.FORWARD, MoveDirection.BACKWARD));
         String[] list_3 = {"l"};
-        MoveDirection[] expected_3 = {MoveDirection.LEFT};
+        LinkedList<MoveDirection> expected_3 = new LinkedList<>(List.of(MoveDirection.LEFT));
         String[] list_4 = {"r", "ww"};
-        MoveDirection[] expected_4 = new MoveDirection[2];
-        expected_4[0] = MoveDirection.RIGHT;
+        LinkedList<MoveDirection> expected_4 =new LinkedList<>(List.of(MoveDirection.RIGHT));
 
 
         //when
-        MoveDirection[] result_1 = OptionsParser.change_string_enum(list_1);
-        MoveDirection[] result_2 = OptionsParser.change_string_enum(list_2);
-        MoveDirection[] result_3 = OptionsParser.change_string_enum(list_3);
-        MoveDirection[] result_4 = OptionsParser.change_string_enum(list_4);
+        LinkedList<MoveDirection> result_1 = OptionsParser.change_string_enum(list_1);
+        LinkedList<MoveDirection> result_2 = OptionsParser.change_string_enum(list_2);
+        LinkedList<MoveDirection> result_3 = OptionsParser.change_string_enum(list_3);
+        LinkedList<MoveDirection> result_4 = OptionsParser.change_string_enum(list_4);
 
         //then
-        assertArrayEquals(expected_1, result_1);
-        assertArrayEquals(expected_2, result_2);
-        assertArrayEquals(expected_3, result_3);
-        assertArrayEquals(expected_4, result_4);
+        assertArrayEquals(expected_1.toArray(), result_1.toArray());
+        assertArrayEquals(expected_2.toArray(), result_2.toArray());
+        assertArrayEquals(expected_3.toArray(), result_3.toArray());
+        assertArrayEquals(expected_4.toArray(), result_4.toArray());
 
     }
 }
