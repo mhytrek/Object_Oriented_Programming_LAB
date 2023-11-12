d7081b7bfd8ef0f17208e11d378c2b7ba7cf06fe
Michalina Hytrek <michalinahytrek@MacBook-Pro-Michalina.local>
2023-11-12 15:39:51 +0100

    Testy do grassfield i rectangularmap

--- oolab/src/test/java/agh/ics/oop/model/GrassFieldTest.java
+++ oolab/src/test/java/agh/ics/oop/model/GrassFieldTest.java
@@ -1,2 +1,79 @@
-package agh.ics.oop.model;public class GrassFieldTest {
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GrassFieldTest {
+
+    @Test
+    public void testCanMoveTo() {
+        //given
+        GrassField grassField = new GrassField(10);
+        Animal animal = new Animal( new Vector2d(2, 2));
+
+        //when
+        grassField.place(animal);
+
+        //then
+        assertTrue(grassField.canMoveTo(new Vector2d(3, 3)));
+        assertFalse(grassField.canMoveTo(new Vector2d(2, 2)));
+    }
+
+    @Test
+    public void testPlace() {
+        //given
+        GrassField grassField = new GrassField(10);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //then
+        assertTrue(grassField.place(animal));
+        assertTrue(grassField.isOccupied(animal.getPosition()));
+        assertFalse(grassField.place(animal));
+    }
+
+    @Test
+    public void testMove() {
+        //given
+        GrassField grassField = new GrassField(10);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        grassField.place(animal);
+
+        // then
+        grassField.move(animal, MoveDirection.FORWARD);
+        assertEquals(new Vector2d(2, 3), animal.getPosition());
+    }
+
+    @Test
+    public void testIsOccupied() {
+        //given
+        GrassField grassField = new GrassField(10);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        grassField.place(animal);
+
+        //then
+        assertTrue(grassField.isOccupied(new Vector2d(2, 2)));
+        assertFalse(grassField.isOccupied(new Vector2d(11, 11)));
+    }
+
+    @Test
+    public void testObjectAt() {
+        //given
+        GrassField grassField = new GrassField(10);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        grassField.place(animal);
+
+        //then
+        assertEquals(animal, grassField.objectAt(new Vector2d(2, 2)));
+        assertNull(grassField.objectAt(new Vector2d(1, 1)));
+    }
+
 }
+
--- oolab/src/test/java/agh/ics/oop/model/RectangularMapTest.java
+++ oolab/src/test/java/agh/ics/oop/model/RectangularMapTest.java
@@ -1,2 +1,77 @@
-package agh.ics.oop.model;public class RectangularMapTest {
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RectangularMapTest {
+
+    @Test
+    public void testPlace() {
+        //given
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //then
+        assertTrue(map.place(animal));
+        assertFalse(map.place(animal));
+    }
+
+    @Test
+    public void testMove() {
+        //given
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        map.place(animal);
+        map.move(animal, MoveDirection.FORWARD);
+
+        //then
+        assertEquals(new Vector2d(2, 3), animal.getPosition());
+    }
+
+    @Test
+    public void testIsOccupied() {
+        //given
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        map.place(animal);
+
+        //then
+        assertTrue(map.isOccupied(new Vector2d(2, 2)));
+        assertFalse(map.isOccupied(new Vector2d(1, 1)));
+    }
+
+    @Test
+    public void testObjectAt() {
+        //given
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        map.place(animal);
+
+
+        //then
+        assertEquals(animal, map.objectAt(new Vector2d(2, 2)));
+        assertNull(map.objectAt(new Vector2d(1, 1)));
+    }
+
+    @Test
+    public void testCanMoveTo() {
+        //given
+        RectangularMap map = new RectangularMap(5, 5);
+        Animal animal = new Animal(new Vector2d(2, 2));
+
+        //when
+        map.place(animal);
+
+        //then
+        assertTrue(map.canMoveTo(new Vector2d(3, 3)));
+        assertFalse(map.canMoveTo(new Vector2d(6, 6))); // Out of map bounds
+        assertFalse(map.canMoveTo(new Vector2d(2, 2))); // Already occupied
+    }
 }
